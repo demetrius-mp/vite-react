f3b5eb63cc231424f423dd2263b6dc83276ebf78
Demetrius Moreira Panovitch <demetrius.mp789@gmail.com>
2022-08-29 12:10:52 -0400

    chore: use vite plugin to replace ts path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
-  },
+  plugins: [react(), tsconfigPaths()],
 });
