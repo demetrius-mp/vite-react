be12093a429bf644f305aa65db95582ff70da5ac
Demetrius Moreira Panovitch <demetrius.mp789@gmail.com>
2022-08-29 12:08:37 -0400

    style: fix eslint rules

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "node:path";
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
 });
